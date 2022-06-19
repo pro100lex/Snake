18eebdd1b6d584a24898493288bc79fd78e42cb3
Alexey <pro100_lex2005@mail.ru>
2022-06-19 21:09:17 +0900

    added variables for the game

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty, ListProperty, BooleanProperty
 
 
 class Playground(Widget):
     fruit = ObjectProperty(None)
     snake = ObjectProperty(None)
+
+    # Размер сетки
+    col_numb = 16
+    row_numb = 8
+
+    # Игровые переменные
+    score = NumericProperty(0)
+    turn_counter = NumericProperty(0)
+    fruit_rythme = NumericProperty(0)
+
+    # Обработка входных данных
+    touch_start_pos = ListProperty()
+    action_triggered = BooleanProperty(False)
 
 
 class Fruit(Widget):
